b3fba8dd44686767de376b13b1fea59cedb2445c
Ryan Golder <rgolder78@gmail.com>
2023-03-12 15:12:44 +1100

    finalise code

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,52 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-const currentDay = $('#currentDay');
-const past = $('.past');
-const present = $('.present');
-const future = $('.future');
+const currentDay = $("#currentDay");
+const timeBlock = $(".time-block");
+const saveButton = $(".saveBtn");
+const description = $(".description");
+const textArea = $("textarea");
+const currentTime = new Date();
+const currentHour = currentTime.getHours();
 
 $(function () {
+  saveButton.on("click", function () {
+    var description = $(this).siblings("textarea").val().trim();
+    var timeBlockId = $(this).parent().attr("id");
+    localStorage.setItem(timeBlockId, description);
+  });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  timeBlock.each(function () {
+    // get the hour value from the element's id
+    var hour = parseInt($(this).attr("id").split("-")[1]);
 
+    // add the appropriate class based on the current time
+    if (hour < currentHour) {
+      $(this).addClass("past");
+    } else if (hour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 
+  $(".time-block").each(function () {
+    var timeBlockId = $(this).attr("id");
+    var savedDescription = localStorage.getItem(timeBlockId);
+    if (savedDescription !== null) {
+      $(this).find("textarea").val(savedDescription);
+    }
+  });
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  $(document).ready(function() {
+  $(document).ready(function () {
     var now = new Date();
-    var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-    var currentDate = now.toLocaleDateString('en-UK', options);
+    var options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    var currentDate = now.toLocaleDateString("en-UK", options);
     currentDay.text(currentDate);
-  });  
+  });
 });
